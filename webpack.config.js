827e17db2d24b59e702735f29cfa9b0f94a2ef16
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-01 15:59:52 -0400

    Set up LiveReload

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,4 +8,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   watch: true,
+  plugins: [
+    new LiveReloadPlugin(),
+  ],
 };
