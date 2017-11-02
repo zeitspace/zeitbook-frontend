03cd9e0110e00fc169c86bf13e17560262a0fcb2
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-02 09:47:03 -0400

    Display posts on main page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-function component() {
-  var element = document.createElement('div');
-  element.innerHTML = 'Hello world';
-  return element;
+import _ from 'lodash';
+
+import {
+  getPosts,
+  createPost,
+} from './api';
+
+// TODO: replace this randomly-generated username with one of your choosing
+const username = _(_.times(6, () => _.random(25)))
+  .map(n => String.fromCharCode(97 + n))
+  .join('');
+
+function buildPostElement({ username, title, body }) {
+  const articleElement = document.createElement('article');
+
+  const titleElement = document.createElement('h2');
+  const titleTextNode = document.createTextNode(title);
+  titleElement.appendChild(titleTextNode);
+
+  const usernameElement = document.createElement('h3');
+  const usernameTextNode = document.createTextNode(`By ${username}`);
+  usernameElement.appendChild(usernameTextNode);
+
+  const bodyElement = document.createElement('p');
+  const bodyTextNode = document.createTextNode(body);
+  bodyElement.appendChild(bodyTextNode);
+
+  articleElement.appendChild(titleElement);
+  articleElement.appendChild(usernameElement);
+  articleElement.appendChild(bodyElement);
+
+  return articleElement;
 }
 
-document.body.appendChild(component());
+const postsContainer = document.getElementById('posts');
+getPosts().then(posts => posts.forEach(post => {
+  postsContainer.appendChild(buildPostElement(post));
+}));
