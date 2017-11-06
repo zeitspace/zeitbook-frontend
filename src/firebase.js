d172521f3a45bc956d787b05fa43c7336b5dba99
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-06 17:39:30 -0500

    Add push notifications

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,47 @@
+import * as firebase from 'firebase/app';
+import 'firebase/messaging';
+
+const config = {
+  apiKey: 'AIzaSyCm6wDitw69vDYVnLcNG91LrV3ClEm_rHc',
+  authDomain: 'zeitspace-forum.firebaseapp.com',
+  databaseURL: 'https://zeitspace-forum.firebaseio.com',
+  projectId: 'zeitspace-forum',
+  storageBucket: 'zeitspace-forum.appspot.com',
+  messagingSenderId: '81782109643',
+};
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
 const registerServiceWorker = navigator.serviceWorker.register('/service-worker.js')
   .then(registration => messaging.useServiceWorker(registration));
 
-export default () => Promise.resolve('TODO: implement push notifications');
+function storeNotificationToken(token) {
+  localStorage.setItem('notificationToken', token);
+}
+
+messaging.onTokenRefresh(() => {
+  messaging.getToken()
+    .then(storeNotificationToken)
+    .catch(error => console.log(error));
+});
+
+function getNotificationToken() {
+  return registerServiceWorker
+    .then(() => new Promise((resolve) => {
+      const storedToken = localStorage.getItem('notificationToken');
+      if (storedToken) {
+        resolve(storedToken);
+      } else {
+        messaging.requestPermission()
+          .then(() => messaging.getToken())
+          .then((token) => {
+            storeNotificationToken(token);
+            resolve(token);
+          })
+          .catch(error => console.log(error));
+      }
+    }));
+}
+
+export default getNotificationToken;
