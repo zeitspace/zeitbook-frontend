8e93bf4a2744650e345b8c920f4856bf02a4722f
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-08 16:00:10 -0500

    Delete trailing spaces

--- src/post.js
+++ src/post.js
@@ -51,7 +51,7 @@ $('#comment-submit').click(() => {
       .then((comment) => {
         noCommentsMessage.hide();
         commentBodyInput.val('');
-        autosize.update(commentBodyInput);        
+        autosize.update(commentBodyInput);
         commentsContainer.append(buildCommentElement(comment));
       })
       .catch(() => {
