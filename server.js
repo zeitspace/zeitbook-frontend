4976f166d1a4a0a312d082c64b4668cfbfa05937
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-11-08 13:52:23 -0500

    Change to match steps

--- server.js
+++ server.js
@@ -16,14 +16,6 @@ app.get('/posts/:id', (request, response) => {
   response.sendFile('assets/pages/post.html', { root: __dirname });
 });
 
-app.get('/manifest.json', (request, response) => {
-  response.sendFile('/assets/manifest.json', { root: __dirname });
-});
-
-app.get('/service-worker.js', (request, response) => {
-  response.sendFile('/assets/javascripts/service-worker.js', { root: __dirname });
-});
-
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('To use Zeitbook, open http://localhost:3000 in your browser.');
